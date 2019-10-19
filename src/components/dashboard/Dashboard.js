77ca409541565677c80a16b255954d3e2f2b2579
Siddharth <siddharth.pandey@accolite.com>
2019-10-19 11:41:43 +0530

    UI completed

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -8,9 +8,9 @@ class Dashboard extends Component {
     render () {
         return (
             <div className="ph2">
-                <div className="fl w-100">
+                <div className="w-100">
                     <Navbar text='Schedule' />
-                    <div className="f6 bg-white pt3 pb3">
+                    <div className="f6 bg-white pt3 pb0">
                         <div className="flex items-center justify-center flex-column mt2 mb2 tc">
                             <div class='cf dib'>
                                 <a class="b b--silver bb bg-washed-white bl br--left br2 bt dib f6 fl link ph2 pv1 red" href="#0">Upcoming</a>
@@ -18,6 +18,13 @@ class Dashboard extends Component {
                                 <a class="b--silver b ba bg-light-white dib f6 fl hover-bg-white hover-red hover-b-silver link ph2 pv1 silver br--right br2" href="#0">Completed</a>
                             </div>
                         </div>
+                        <nav class="pa3 pa4-ns pb0">
+                            <div class="tc pb2">
+                                <a class="link dim gray f6 f5-ns dib mr5" href="#" title="Home">All</a>
+                                <a class="link dim gray f6 f5-ns dib mr4" href="#" title="About">International</a>
+                                <a class="link dim gray f6 f5-ns dib" href="#" title="Store">Domestic</a>
+                            </div>
+                        </nav>
                     </div>
                     <ScheduleBox />
                     <ScheduleBox />
--- src/components/scheduleBox/ScheduleBox.js
+++ src/components/scheduleBox/ScheduleBox.js
@@ -3,19 +3,25 @@ import { Link } from 'react-router-dom';
 
 const ScheduleBox = (props) => {
     return (
-        <div className="fl measure bg-white pb3 pr3 pl3 mt0 mb3">
-            <Link to='/' className="fl w-100 mt2 mb2 bg-light-black">
-                <div>
-                    <div className="fl f6 fw6 black-80">Series</div>
-                    <div className="fr f5 fw4 black-40"> > </div>
-                </div>
-            </Link>
-            <p className="fl mt2 mb2 f6 fw6 black-80">Match 10 - Location</p>
-            <div className="w-100 fl mt2 mb2">
-                <div className="fl f6 black-80">Icon</div>
-                <div className="fl ml2 f6 fw6 black-80">Location</div>
+        <div className="measure bg-white pb3 pr3 pl3 mt0 mb3 pt1">
+            <div className="pb2 pb4 pt1 pt2 w-100 bg-grey">
+                <Link to='/' className="">
+                    <div className="fl f6 fw6 black-80 dib">Series</div>
+                    <div className="fr f5 fw4 black-40 dib"> > </div>
+                </Link>
             </div>
-            <div className="fl w-100 pt2 pb2 tc bg-washed-red">15 mins to toss</div>
+            <div className="mt2 mb2 f6 fw6 black-80">Match 10 - Location</div>
+            <div className="mt2 mb2">
+                <div className="f6 black-80 dib">Icon</div>
+                <div className="ml2 f6 fw6 black-80 dib">Location</div>
+            </div>
+            <div className="mt2 mb2">
+                <div className="f6 black-80 dib">Icon</div>
+                <div className="ml2 f6 fw6 black-80 dib">Location</div>
+            </div>
+            <div className="flex items-center justify-center flex-column mt3">
+                <div className="w-60 tc pt1 pb1 br4 bg-washed-red">15 mins to toss</div>
+            </div>
         </div>
     );
 }
